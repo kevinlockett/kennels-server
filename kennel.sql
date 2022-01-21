0cb63233f86541485bed657b8b4eb5f2e8d65716
Kevin Lockett <chlockett@msn.com>
2022-01-20 20:58:19 -0600

    kll-Chapter 14 completed

--- kennel.sql
+++ kennel.sql
@@ -73,6 +73,8 @@ SELECT * FROM Location;
 
 SELECT * FROM Animal;
 
+SELECT * FROM Employee;
+
 INSERT INTO `Animal` VALUES (null, "Daps", "Kennel", "Boxer", 2, 2);
 
 UPDATE Customer
@@ -86,6 +88,35 @@ UPDATE Employee
 UPDATE Location
 	SET address = "64 Washington Heights"
 	WHERE name = "Nashville North";
+
+SELECT
+    a.id,
+    a.name,
+    a.breed,
+    a.status,
+    a.location_id,
+    a.customer_id,
+    l.name location_name,
+    l.address location_address,
+	c.name customer_name,
+	c.address customer_address,
+	c.email customer_email
+FROM Animal a
+JOIN Location l
+    ON l.id = a.location_id
+JOIN Customer c
+	ON c.id = a.customer_id
+
+SELECT
+	e.id,
+	e.name,
+	e.address,
+	e.location_id,
+	l.name location_name,
+	l.address location_address
+FROM Employee e
+JOIN Location l
+	ON l.id = e.location_id
 
 
 
